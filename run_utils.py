7008a36c88992eccb2b3867655d33c760dcb54c7
Denis Volk <denis.volk@gmail.com>
2022-07-12 10:38:57 +0100

    change run cmd columns to 100

--- run_utils.py
+++ run_utils.py
@@ -11,9 +11,9 @@ term = "xterm"
 
 def run_term_ssh_cmd(hostname, username, password, ssh_cmd):
     """Run command remotely, opening a local terminal."""
-    term_cmd = f"sshpass -p {shlex.quote(password)} ssh -oUserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no {shlex.quote(username)}@{shlex.quote(hostname)} {shlex.quote('TERM=xterm-256color ' + ssh_cmd)} ; read -n 1 -p 'Command finished. Press any key to continue.'"
+    term_cmd = f"sshpass -p {shlex.quote(password)} ssh -oUserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no {shlex.quote(username)}@{shlex.quote(hostname)} {shlex.quote('COLUMNS=100 TERM=xterm-256color ' + ssh_cmd)} ; read -n 1 -p 'Command finished. Press any key to continue.'"
     logging.info(term_cmd)
-    subprocess.Popen([term, "-e", term_cmd])
+    subprocess.Popen([term, "-geometry", "100x24", "-e", term_cmd])
 
 
 def run_term_rsync_up(hostname, username, password, local_path, remote_path):
